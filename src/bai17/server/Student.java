efdb4492a60baaff0d445499fc947a224a382f32
Lam Nguyen <kiminonawa1305@gmail.com>
2023-12-28 08:18:22 +0700

    Hoàn thành bài 17

---
+++ src/bai17/server/Student.java
@@ -0,0 +1,59 @@
+package bai17.server;
+
+public class Student {
+    private String id, name;
+    private int age;
+    private double socre;
+
+    public Student(String id, String name, int age, double socre) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.socre = socre;
+    }
+
+    public Student() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSocre() {
+        return socre;
+    }
+
+    public void setSocre(double socre) {
+        this.socre = socre;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", socre=" + socre +
+                '}';
+    }
+}
---
+++ src/bai17/server/User.java
@@ -0,0 +1,10 @@
+package bai17.server;
+
+public class User {
+    private String userName, password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
